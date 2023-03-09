aa396de63e81915a52778e8fe42461b8e7636508
Nreker2442 <96087399+Nreker2442@users.noreply.github.com>
2023-03-09 11:22:16 -0600

    Update streamlit_app.py
    added second header and items

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,10 @@ streamlit.text('Pourover')
 streamlit.text('Drip')
 streamlit.text('Americano')
 streamlit.text('Cortado')
+
+
+streamlit.header('Food Menu')
+streamlit.text('Blueberry Scone')
+streamlit.text('Chocolate Chip Cookie')
+streamlit.text('Banana Bread')
+streamlit.text('Granola Bar')

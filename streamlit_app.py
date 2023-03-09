0779c96365052123389f6faa722200224b63a408
Nreker2442 <96087399+Nreker2442@users.noreply.github.com>
2023-03-09 11:20:14 -0600

    Update streamlit_app.py
    added header and items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('Our New Coffee Shop')
+
+streamlit.header('Drink Menu')
+streamlit.text('Pourover')
+streamlit.text('Drip')
+streamlit.text('Americano')
+streamlit.text('Cortado')

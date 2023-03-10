e8cb254ec93044178b2cd42d1e1693e46ec8cb98
Nreker2442 <96087399+Nreker2442@users.noreply.github.com>
2023-03-10 17:43:00 -0600

    Update streamlit_app.py
    add input box for fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,12 +28,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 strealit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #writes data to the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_noramlized)
+streamlit.dataframe(fruityvice_normalized)
 
 
